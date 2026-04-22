ef3882f32d9056d0d54fcc9edc97dda8cc744f49
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-22 19:09:53 -0400

    added classes for styling

--- src/components/UserProfileCard/UserProfileCard.tsx
+++ src/components/UserProfileCard/UserProfileCard.tsx
@@ -3,11 +3,13 @@ import type { UserProfileCardProps } from '../../types/index.ts';
 function UserProfileCard({ user, showEmail = true, showRole = true, onEdit, children }: UserProfileCardProps) {
     return (
         <div className="user-profile-card">
-            <img src={user.avatarUrl} alt={`${user.name}'s avatar`} />
-            <h2>{user.name}</h2>
-            {showEmail && <p>{user.email}</p>}
-            {showRole && <p>{user.role}</p>}
-            {onEdit && <button onClick={() => onEdit(user.id)}>Edit</button>}
+            <div className='content'>
+                <img src={user.avatarUrl} alt={`${user.name}'s avatar`} className='avatar' />
+                <h2>{user.name}</h2>
+                {showEmail && <p className='email'>{user.email}</p>}
+                {showRole && <p className='role'>{user.role}</p>}
+            </div>
+            {onEdit && <button onClick={() => onEdit(user.id)} className='edit'>Edit</button>}
             {children}
         </div>
     )
