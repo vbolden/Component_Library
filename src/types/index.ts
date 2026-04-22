a8f72391c7a2ad05c96cce8fcaca4f72b563cc5b
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-22 11:48:55 -0400

    added user profile interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,19 @@ export interface AlertBoxProps {
   onClose?: () => void;
   children?: React.ReactNode;
 }
+
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    role: string;
+    avatarUrl?: string;
+}
+
+export interface UserProfileCardProps {
+    user: User;
+    showEmail?: boolean;
+    showRole?: boolean;
+    onEdit?: (userId: string) => void;
+    children?: React.ReactNode;
+}
