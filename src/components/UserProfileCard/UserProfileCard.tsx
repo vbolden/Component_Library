efb29cd63923e561ebde4e34aeb5237dd19f92c6
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-22 16:59:37 -0400

    formatted file

--- src/components/UserProfileCard/UserProfileCard.tsx
+++ src/components/UserProfileCard/UserProfileCard.tsx
@@ -1,9 +1,9 @@
-import type {UserProfileCardProps} from '../../types/index.ts';
+import type { UserProfileCardProps } from '../../types/index.ts';
 
-function UserProfileCard ({ user, showEmail = true, showRole = true, onEdit, children }: UserProfileCardProps) {
+function UserProfileCard({ user, showEmail = true, showRole = true, onEdit, children }: UserProfileCardProps) {
     return (
         <div className="user-profile-card">
-            <img src={user.avatarUrl} alt={`${user.name}'s avatar}`} />
+            <img src={user.avatarUrl} alt={`${user.name}'s avatar`} />
             <h2>{user.name}</h2>
             {showEmail && <p>{user.email}</p>}
             {showRole && <p>{user.role}</p>}
