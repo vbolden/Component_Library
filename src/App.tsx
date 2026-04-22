df763ae28bedeef62f3d2c1f74bd2c0f9040edbc
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-22 17:00:18 -0400

    added UserProfileCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AlertBox from './components/AlertBox/AlertBox';
+import ProductDisplay from './components/ProductDisplay/ProductDisplay';
+import UserProfileCard from './components/UserProfileCard/UserProfileCard';
+
+const user = {
+  id: '1',
+  name: 'Abigail Smith',
+  email: 'abagail.smith@example.com',
+  role: 'Full Stack Engineer',
+  avatarUrl: 'https://example.com/avatar.jpg'
+}
 
 function App() {
+  const [showAlert, setShowAlert] = useState(false);
 
   return (
-    <>
-    </>
+    <div>
+      {showAlert && (
+        <AlertBox
+          type='success'
+          message='Your profile has been updated successfully!'
+          onClose={() => alert('Alert closed')}
+        />
+      )}
+
+      <div className="container">
+        <UserProfileCard
+          user={user}
+          showEmail={true}
+          showRole={true}
+        />
+
+        {/* <ProductDisplay
+          product={product}
+          showDescription={true}
+          showStockStatus={true}
+          onAddToCart={handleAddtoCart}
+        /> */}
+      </div>
+
+    </div>
   )
 }
 
